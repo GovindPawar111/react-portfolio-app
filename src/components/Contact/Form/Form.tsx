cfe48b1efb0c948ada7a9d404bc650c82ac7cc02
Govind Pawar <pawargovind511@gmail.com>
2023-05-21 15:16:32 +0530

    completed contact section

--- src/components/Contact/Form/Form.tsx
+++ src/components/Contact/Form/Form.tsx
@@ -11,9 +11,9 @@ const templateId = import.meta.env.VITE_PORTFOLIO_TEMPLATE_ID
 const userId = import.meta.env.VITE_PORTFOLIO_USER_ID
 
 type FormData = {
-    'from-name': string
-    'reply-to': string
-    'phone-number': string
+    from_name: string
+    reply_to: string
+    phone_number: string
     subject: string
     message: string
 }
@@ -53,65 +53,96 @@ const Form: React.FC = () => {
     return (
         <div className="query-form">
             <ToastContainer />
-            <form id="queryForm" onSubmit={handleSubmit(submitHandler)}>
+            <form
+                id="queryForm"
+                onSubmit={handleSubmit(submitHandler)}
+                className="form"
+            >
                 <div className="input-field">
                     <input
                         type="text"
                         placeholder="Name"
-                        {...register('from-name', {
-                            required: true,
+                        {...register('from_name', {
+                            required: {
+                                value: true,
+                                message: 'Name is required',
+                            },
                             maxLength: {
                                 value: 16,
                                 message: 'Maximum 16 characters are allowed',
                             },
                         })}
                     />
+                    {errors.from_name?.message && (
+                        <p className="error">{errors.from_name?.message}</p>
+                    )}
                 </div>
                 <div className="input-field">
                     <input
                         type="text"
                         placeholder="Email"
-                        {...register('reply-to', {
-                            required: true,
+                        {...register('reply_to', {
+                            required: {
+                                value: true,
+                                message: 'Email is required',
+                            },
                             pattern: {
                                 value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
                                 message: 'Invalid email address',
                             },
                         })}
                     />
+                    {errors.reply_to?.message && (
+                        <p className="error">{errors.reply_to?.message}</p>
+                    )}
                 </div>
                 <div className="input-field">
                     <input
                         type="text"
                         placeholder="Phone number"
-                        {...register('phone-number', {
-                            required: true,
+                        {...register('phone_number', {
+                            required: {
+                                value: true,
+                                message: 'Phone number is required',
+                            },
                             minLength: {
                                 value: 8,
                                 message: 'Phone number is not valid',
                             },
                         })}
                     />
+                    {errors.phone_number?.message && (
+                        <p className="error">{errors.phone_number?.message}</p>
+                    )}
                 </div>
                 <div className="input-field">
                     <input
                         type="text"
                         placeholder="Subject"
                         {...register('subject', {
-                            required: true,
+                            required: {
+                                value: true,
+                                message: 'Subject is required',
+                            },
                             minLength: {
                                 value: 10,
                                 message: 'Minimum 10 characters required',
                             },
                         })}
                     />
+                    {errors.subject?.message && (
+                        <p className="error">{errors.subject?.message}</p>
+                    )}
                 </div>
                 <div className="input-field full-width">
                     <textarea
                         className="textarea"
                         placeholder="Your message"
                         {...register('message', {
-                            required: true,
+                            required: {
+                                value: true,
+                                message: 'text message is required',
+                            },
                             minLength: {
                                 value: 20,
                                 message: 'Minimum 20 characters required',
@@ -122,6 +153,9 @@ const Form: React.FC = () => {
                             },
                         })}
                     />
+                    {errors.message?.message && (
+                        <p className="error">{errors.message?.message}</p>
+                    )}
                 </div>
 
                 <button type="submit">Submit</button>
