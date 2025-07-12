071a259eb8fe0d5a93c3de7026d3d156bd4093e4
Govind Pawar <pawargovind512@gmail.com>
2025-07-12 19:18:05 +0530

    Removed unnecessary App.scss files and rename index.scss to main.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.scss'
 import About from './components/About/About'
 import Intro from './components/Intro/Intro'
 import Skills from './components/Skills/Skills'
@@ -6,7 +5,6 @@ import Experience from './components/Experience/Experience'
 import Portfolio from './components/Portfolio/Portfolio'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
-// import Blogs from './components/Blogs/Blogs'
 
 export const App = (): React.ReactElement => {
     return (
